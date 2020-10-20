d2955c09227728321af5963f1bb0b8015fbdf1c3
intade <dgolovenko01@gmail.com>
2020-10-20 23:33:26 +0300

    lab3

---
+++ lab3/code.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <omp.h>
+#include <ctime>
+#include <random>
+
+void init(int** matrix, int* vector, int row, int col){
+    for(int i = 0; i < row; i++){
+        matrix[i] = new int[col];
+        for(int j = 0; j < col; j++){
+            matrix[i][j] = rand(); 
+            vector[j] = rand();
+        }
+    }
+}
+int* mulOMP(int** matrix, int* vector, int row, int col){
+    int* result = new int[row];
+    #pragma omp parallel 
+    {
+        int i;
+        #pragma omp for 
+        for(i =0; i < row; i++){
+            result[i] = 0;
+            for(int j = 0; j < col; j++ ){
+                result[i] += vector[j] * matrix[i][j];
+            }
+        } 
+    }
+    return result;
+}
+
+int* mul(int** matrix, int* vector, int row, int col){
+    int* result = new int[row];
+    int i;
+    for(i =0; i < row; i++){
+        result[i] = 0;
+        for(int j = 0; j < col; j++ ){
+            result[i] += vector[j] * matrix[i][j];
+        }
+    } 
+    return result;
+}
+int main(){
+    srand(time(0));
+    int row = 10000;
+    int col = 10000;
+    int** matrix = new int*[row];
+    int* vector = new int[col];
+    init(matrix, vector, row, col);
+    float time_start, time_end;
+    int* result;
+
+    time_start = omp_get_wtime(); 
+    result = mul(matrix, vector, row, col);
+    time_end = omp_get_wtime();
+    std::cout << "without omp\t" << time_end - time_start << '\n';
+
+    delete result;
+
+    time_start = omp_get_wtime(); 
+    result = mulOMP(matrix, vector, row, col);
+    time_end = omp_get_wtime();
+    std::cout << "with omp\t" << time_end - time_start << '\n';
+
+    for(int i =0; i < row; i++){
+        delete matrix[i];
+    }
+    delete result, vector, matrix;
+
+    return 0;
+}
+
+
